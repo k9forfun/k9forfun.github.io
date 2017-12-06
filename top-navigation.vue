10915dbd863ae665a13807115455a70426f1b016
Johan Iverås <johan.iveras@tui.se>
2017-12-06 16:34:08 +0100

    small updates.

--- top-navigation.vue
+++ top-navigation.vue
@@ -17,11 +17,7 @@
             <a class="nav-link">Bar</a>
           </router-link>
           
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        </ul>        
       </div>
     </nav>
 </template>
